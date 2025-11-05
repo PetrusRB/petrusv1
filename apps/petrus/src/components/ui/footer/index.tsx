61be06f9f84fa7dafaec890726bce5159174f27a
PetrusRB <pedro.co.dev.ac@gmail.com>
2025-11-05 17:07:39 -0400

    remove code2 on footer.tsx

--- apps/petrus/src/components/ui/footer/index.tsx
+++ apps/petrus/src/components/ui/footer/index.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import { Code2, Heart, Github, Twitter, Mail } from 'lucide-react';
+import { Heart, Github, Twitter } from 'lucide-react';
 import Image from 'next/image';
 export const Footer = () => {
   return (
